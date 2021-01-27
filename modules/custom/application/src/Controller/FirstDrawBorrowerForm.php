8fca64ca52797d5b7833e8aec6ff60577a51a062
IsamuYun <yunchunnan@qq.com>
2021-01-26 16:14:15 -0800

    Update TIN

--- modules/custom/application/src/Controller/FirstDrawBorrowerForm.php
+++ modules/custom/application/src/Controller/FirstDrawBorrowerForm.php
@@ -253,7 +253,7 @@ class FirstDrawBorrowerForm {
             'document_id' => "1", "page_number" => "1",
             "x_position" => "365", "y_position" => "402",
             "font" => "Arial", "font_size" => "size8",
-            "value" => $controller->getTIN(),
+            "value" => $this->elements["social_security_number"]["#default_value"],
             "height" => "10", "width" => "60", "required" => "false"
         ]);
  
