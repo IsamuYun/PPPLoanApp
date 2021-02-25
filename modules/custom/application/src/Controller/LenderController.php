476445a584b26d058fa65f04b40681b8e57ed248
IsamuYun <yunchunnan@qq.com>
2021-02-25 11:40:41 -0800

    Update Lender Form

--- modules/custom/application/src/Controller/LenderController.php
+++ modules/custom/application/src/Controller/LenderController.php
@@ -8,6 +8,8 @@ use setasign\Fpdi\PdfParser\PdfParserException;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\webform\Utility\WebformFormHelper;
 
+use Drupal\application\Controller\SecondLenderController;
+
 use Drupal\file\FileInterface;
 
 /**
@@ -29,6 +31,12 @@ class LenderController {
 
         $elements = WebformFormHelper::flattenElements($form);
 
+        if ($elements["round"]["#default_value"] == "Yes") {
+            $controller = new SecondLenderController();
+            $controller->createLenderFormPDF($form, $form_state);
+            return;
+        }
+
         try {
             $pageCount = $pdf->setSourceFile(self::DOC_PATH . "PPP Lender Application Form-508.pdf");
             $templateId = $pdf->importPage(1);
@@ -39,17 +47,17 @@ class LenderController {
             $this->printCompanyStructure($pdf, $elements);
             $this->printBorrowerInformation($pdf, $elements);
 
-            $this->printEligibility1($pdf, $elements);
-            
             $templateId = $pdf->importPage(2);
             $pdf->AddPage();
             $pdf->useTemplate($templateId);
+            $this->printEligibility1($pdf, $elements);
             $this->printEligibility2($pdf, $elements);
-            $this->printLenderCertification($pdf, $elements);
+            
 
             $templateId = $pdf->importPage(3);
             $pdf->AddPage();
             $pdf->useTemplate($templateId);
+            $this->printLenderCertification($pdf, $elements);
 
             $submission_id = $form_state->getFormObject()->getEntity()->id();
             
@@ -110,18 +118,18 @@ class LenderController {
         $pdf->SetXY(162, 67);
         $pdf->Write(0, "530223");
         // Lender Address
-        $pdf->SetFont('Helvetica', '', 10);
-        $pdf->SetXY(36, 73);
+        $pdf->SetFont('Helvetica', '', 9);
+        $pdf->SetXY(36, 72);
         $pdf->Write(0, "1 World Trade Center, Suite 1130");
         // City
-        $pdf->SetFont('Helvetica', '', 12);
-        $pdf->SetXY(98, 73);
+        $pdf->SetFont('Helvetica', '', 10);
+        $pdf->SetXY(98, 72);
         $pdf->Write(0, "Long Beach");
         // State
-        $pdf->SetXY(140, 73);
+        $pdf->SetXY(140, 72);
         $pdf->Write(0, "CA");
         // Zip 
-        $pdf->SetXY(170, 73);
+        $pdf->SetXY(170, 72);
         $pdf->Write(0, "90831");
         // Lender Contact
         $pdf->SetFont('Helvetica', '', 10);
@@ -137,13 +145,41 @@ class LenderController {
         $pdf->SetXY(36, 83);
         $pdf->Write(0, $email);
         // Title
-        $pdf->SetXY(124, 83);
+        $pdf->SetXY(130, 83);
         $pdf->Write(0, "Admin");
     }
 
     private function printCompanyStructure($pdf, $elements) {
         $check = "3";
         $pdf->SetFont('ZapfDingbats','', 10);
+        /*
+        $pdf->SetXY(39, 91);
+        $pdf->Cell(10, 10, $check, 0, 0);
+        $pdf->SetXY(64.5, 91);
+        $pdf->Cell(10, 10, $check, 0, 0);
+        $pdf->SetXY(85, 91);
+        $pdf->Cell(10, 10, $check, 0, 0);
+        $pdf->SetXY(102, 91);
+        $pdf->Cell(10, 10, $check, 0, 0);
+        $pdf->SetXY(117, 91);
+        $pdf->Cell(10, 10, $check, 0, 0);
+        $pdf->SetXY(128.5, 91);
+        $pdf->Cell(10, 10, $check, 0, 0);
+        $pdf->SetXY(40, 94.5);
+        $pdf->Cell(10, 10, $check, 0, 0);
+        $pdf->SetXY(79, 94.5);
+        $pdf->Cell(10, 10, $check, 0, 0);
+        $pdf->SetXY(110, 94.5);
+        $pdf->Cell(10, 10, $check, 0, 0);
+        $pdf->SetXY(40, 98);
+        $pdf->Cell(10, 10, $check, 0, 0);
+        $pdf->SetXY(87.5, 98);
+        $pdf->Cell(10, 10, $check, 0, 0);
+        $pdf->SetXY(120, 98);
+        $pdf->Cell(10, 10, $check, 0, 0);
+        $pdf->SetXY(143.5, 98);
+        $pdf->Cell(10, 10, $check, 0, 0);
+        */
         
         $company_structure = $elements["company_structure"]["#default_value"];
         if ($company_structure == "Sole Proprietorship") {
@@ -152,122 +188,192 @@ class LenderController {
         else if ($company_structure == "General Partnership") {
             $pdf->SetXY(64.5, 91);
         }
-        else if ($company_structure == "Limited Liability Company") {
-            $pdf->SetXY(116, 91);
-        }
-        else if ($company_structure == "S Corporation") {
-            $pdf->SetXY(85, 91);
-        }
         else if ($company_structure == "C Corporation") {
             $pdf->SetXY(85, 91);
         }
-        else if ($company_structure == "Other") {
-            $pdf->SetXY(117, 98);
+        else if ($company_structure == "S Corporation") {
+            $pdf->SetXY(102, 91);
+        }
+        else if ($company_structure == "Limited Liability Company") {
+            $pdf->SetXY(116, 91);
+        }
+        else if ($company_structure == "Independent Contractor") {
+            $pdf->SetXY(128.5, 91);
+        }
+        else if ($company_structure == "Eligible Self-employed Individual") {
+            $pdf->SetXY(40, 94.5);
         }
         else if ($company_structure == "501 (c)(3) nonprofit") {
-            $pdf->SetXY(88, 94.5);
+            $pdf->SetXY(79, 94.5);
+        }
+        else if ($company_structure == "501 (c)(6) organization") {
+            $pdf->SetXY(110, 94.5);
         }
         else if ($company_structure == "501 (c)(19) veterans organization") {
-            $pdf->SetXY(119, 94.5);
+            $pdf->SetXY(40, 98);
         }
-        else if ($company_structure == "Tribal business (sec. 31 (b)(2)(c) of Small Business Act)") {
-            $pdf->SetXY(39, 98);
+        else if ($company_structure == "Housing cooperative") {
+            $pdf->SetXY(87.5, 98);
         }
-        else if ($company_structure == "Independent contractor") {
-            $pdf->SetXY(128, 91);
+        else if ($company_structure == "Tribal Business") {
+            $pdf->SetXY(120, 98);
         }
-        else if ($company_structure == "Eligible self-employed individual") {
-            $pdf->SetXY(39, 94.5);
+        else if ($company_structure == "Other") {
+            $pdf->SetXY(143.5, 98);
         }
         $pdf->Cell(10, 10, $check, 0, 0);
+    }
+
+    private function getLoanAmount($elements) {
+        $amount = $elements["adjusted_loan_amount"]["#default_value"];
+        $amount = str_replace(["$", ",", " "], "", $amount);
+        return number_format($amount, 2);
+    }
+
+    private function getAveragePayroll($elements) {
+        $amount = $elements["adjusted_average_payroll"]["#default_value"];
+        $amount = str_replace(["$", ",", " "], "", $amount);
+        $amount *= 2.5;
+        return number_format($amount, 2);
+    }
+
+    private function getLoanTotal($elements) {
+        $payroll = $elements["adjusted_average_payroll"]["#default_value"];
+        $payroll = str_replace(["$", ",", " "], "", $payroll);
+        $payroll *= 2.5;
+
+        $used_EIDL_amount = $elements["used_loan_amount"]["#default_value"];
+        if (empty($used_EIDL_amount)) {
+            $used_EIDL_amount = 0;
+        }
+        $used_EIDL_amount = str_replace(["$", ",", " "], "", $used_EIDL_amount);
+        $amount = 0;
+        if ($payroll >= $used_EIDL_amount) {
+            $amount = $payroll - $used_EIDL_amount;
+        }
+        return number_format($amount, 2);
+    }
+
+    private function getEIDLAmount($elements) {
+        $used_EIDL_amount = $elements["used_loan_amount"]["#default_value"];
+        if (empty($used_EIDL_amount)) {
+            return "0.00";
+        }
+        $used_EIDL_amount = floatval(substr($used_EIDL_amount, 2));
+        return number_format($used_EIDL_amount, 2);
     }
 
     private function printBorrowerInformation($pdf, $elements) {
         $applicant_legal_name = $elements["business_name"]["#default_value"];
         $dba_name = $elements["another_business_name"]["#default_value"];
+        $naics_code = $elements["naics_code"]["#default_value"];
         $tax_id = $elements["tax_id_number"]["#default_value"];
+        $year_of_establishment = $elements["date_established"]["#value"]["year"];
+        $number_of_employees = $elements["number_of_employees"]["#default_value"];
         $address = $this->getBusinessAddress($elements);
         $city_state_zip = $this->getCityStateZip($elements);
         $primary_contact = $this->getPrimaryName($elements);
         $phone_num = $elements["business_phone_number"]["#default_value"];
-        $average_payroll = number_format($this->getAveragePayroll($elements) * 2.5, 2);
+        $average_payroll = $this->getAveragePayroll($elements);
         $loan_amount = $this->getLoanAmount($elements);
         $eidl_amount = $this->getEIDLAmount($elements);
         $pdf->SetFont('Helvetica', '', 10);
-        $pdf->SetXY(66, 113);
+        $pdf->setXY(50, 108.5);
         $pdf->Write(0, $applicant_legal_name);
 
-        $pdf->SetXY(66, 118.5);
+        $pdf->setXY(140, 108.5);
+        $pdf->Write(0, $naics_code);
+
+        $pdf->setXY(66, 113.5);
         $pdf->Write(0, $dba_name);
 
-        $pdf->SetXY(160, 118.5);
+        $pdf->SetXY(140, 113.5);
         $pdf->Write(0, $tax_id);
 
-        $pdf->SetXY(66, 124.5);
+        $pdf->setXY(66, 118.5);
+        $pdf->Write(0, $year_of_establishment);
+
+        $pdf->setXY(140, 118.5);
+        $pdf->Write(0, $number_of_employees);
+
+        $check = "3";
+        $pdf->SetFont('ZapfDingbats','', 10);
+        $pdf->SetXY(71, 118);
+        $pdf->Cell(10, 10, $check, 0, 0);
+
+        $pdf->SetFont('Helvetica', '', 9);
+        $pdf->SetXY(62, 136.5);
         $pdf->Write(0, $address);
 
-        $pdf->SetFont('Helvetica', '', 10);
-        $pdf->SetXY(144, 124.5);
+        $pdf->SetXY(132, 136.5);
         $pdf->Write(0, $city_state_zip);
 
-        $pdf->SetXY(66, 131);
+        $pdf->SetXY(54, 142.5);
         $pdf->Write(0, $primary_contact);
 
-        $pdf->SetXY(154, 131);
+        $pdf->SetXY(120, 142.5);
         $pdf->Write(0, $phone_num);
 
         $pdf->SetFont('Helvetica', '', 10);
-        $pdf->SetXY(50, 141);
+        $pdf->SetXY(54, 157);
         $pdf->Write(0, $loan_amount);
 
-        $pdf->SetXY(155, 166);
+        $pdf->SetXY(178, 181.5);
         $pdf->Write(0, $average_payroll);
         
-        $pdf->SetXY(155, 172);
+        $pdf->SetXY(178, 188);
         $pdf->Write(0, $eidl_amount);
         
-        $pdf->SetXY(155, 178);
-        $pdf->Write(0, $loan_amount);
+        $pdf->SetXY(178, 194);
+        $pdf->Write(0, $this->getLoanTotal($elements));
     }
 
     private function printEligibility1($pdf, $elements) {
         $check = "n";
         $pdf->SetFont('ZapfDingbats','', 11);
-        $pdf->SetXY(177, 196);
+        $pdf->SetXY(177, 24.5);
         $pdf->Cell(10, 10, $check, 0, 0);
 
-        $pdf->SetXY(177, 214);
+        $pdf->SetXY(177, 62);
         $pdf->Cell(10, 10, $check, 0, 0);
 
-        $pdf->SetXY(188, 230);
+        $pdf->SetXY(177, 96);
         $pdf->Cell(10, 10, $check, 0, 0);
 
-        $pdf->SetXY(170, 241);
+        $pdf->SetXY(177.5, 136);
         $pdf->Cell(10, 10, $check, 0, 0);
 
+        $pdf->SetXY(177.5, 140);
+        $pdf->Cell(10, 10, $check, 0, 0);
+
+        $f_code = $elements["sba_franchise_identifier_code"]["#default_value"];
+        $pdf->SetFont('Helvetica', '', 8);
+        $pdf->SetXY(88, 151);
+        $pdf->Write(0, $f_code);
     }
 
     private function printEligibility2($pdf, $elements) {
         $check = "n";
         $pdf->SetFont('ZapfDingbats','', 11);
-        $pdf->SetXY(177, 15.5);
+        
+
+        $pdf->SetXY(177.5, 162);
         $pdf->Cell(10, 10, $check, 0, 0);
 
-        $pdf->SetXY(177, 33);
+        $pdf->SetXY(177.5, 178);
         $pdf->Cell(10, 10, $check, 0, 0);
 
-        $pdf->SetXY(173, 56.5);
+        $pdf->SetXY(173.5, 200.5);
         $pdf->Cell(10, 10, $check, 0, 0);
 
-        $pdf->SetXY(173, 72);
+        $pdf->SetXY(173.5, 212.5);
         $pdf->Cell(10, 10, $check, 0, 0);
 
-        $pdf->SetXY(173.5, 91);
+        $pdf->SetXY(174, 229.5);
         $pdf->Cell(10, 10, $check, 0, 0);
 
-        $pdf->SetXY(192, 107);
+        $pdf->SetXY(190, 245);
         $pdf->Cell(10, 10, $check, 0, 0);
-
     }
 
     private function printLenderCertification($pdf, $elements) {
@@ -279,17 +385,17 @@ class LenderController {
         // extract dimensions from image
         // $info = getimagesize($pic);
 
-        $pdf->Image($pic, 66, 177, 36, 10, 'png');
+        $pdf->Image($pic, 72, 70, 30, 8, 'png');
 
-        $pdf->SetFont('Helvetica', '', 14);
-        $pdf->SetXY(156, 184);
+        $pdf->SetFont('Helvetica', '', 12);
+        $pdf->SetXY(156, 76);
         $pdf->Write(0, date("m-j-Y"));
 
-        $pdf->SetXY(66, 195);
+        $pdf->SetXY(66, 86.5);
         $pdf->Write(0, "Stella Zhang");
 
         $pdf->SetFont('Helvetica', '', 10);
-        $pdf->SetXY(156, 196);
+        $pdf->SetXY(156, 86.5);
         $pdf->Write(0, "Chief Operating Officer");
 
     }
@@ -326,82 +432,12 @@ class LenderController {
     }
 
     private function getPrimaryName($elements) {
-        return $elements["first_name"]["#default_value"] . " " . $elements["last_name"]["#default_value"];
+        return $elements["first_name"][0]["#default_value"] . " " . $elements["last_name"][0]["#default_value"];
     }
 
-    private function getAveragePayroll($elements) {
-        $number_of_employees = $elements["number_of_employees"]["#default_value"];
-        $net_earnings = $elements["net_earnings"]["#default_value"];
-        $net_earnings = str_replace(",", "", $net_earnings);
-        $net_earnings = floatval(substr($net_earnings, 2));
-        $average_payroll = 0;
-        
-        if ($net_earnings > 100000) {
-            $net_earnings = 100000;
-        }
-        else if ($net_earnings < 0) {
-            $net_earnings = 0;
-        }
-        
-        $total_payroll = $elements["total_salaries"]["#default_value"];
-        $total_tax_paid = $elements["total_tax_paid"]["#default_value"];
-        $total_payroll = str_replace(",", "", $total_payroll);
-        $total_tax_paid = str_replace(",", "", $total_tax_paid);
-        $total_payroll = floatval(substr($total_payroll, 2));
-        $total_tax_paid = floatval(substr($total_tax_paid, 2));
-
-        if ($total_payroll > ($number_of_employees * 100000)) {
-            $total_payroll = $number_of_employees * 100000;
-        }
-        else if ($total_payroll < 0) {
-            $total_payroll = 0;
-        }
-
-        if ($total_tax_paid > ($number_of_employees * 100000)) {
-            $total_tax_paid = $number_of_employees * 100000;
-        }
-        else if ($total_tax_paid < 0) {
-            $total_tax_paid = 0;
-        }
-        
-        if ($number_of_employees <= 1) {
-            $average_payroll = $net_earnings / 12;
-        }
-        else {
-            $average_payroll = ($net_earnings + $total_payroll + $total_tax_paid) / 12;
-        }
-
-        return $average_payroll;
-    }
-
-    private function getLoanAmount($elements) {
-        $average_payroll = $this->getAveragePayroll($elements);
-
-        $loan_amount = 0;
-
-        $used_EIDL_amount = $elements["used_loan_amount"]["#default_value"];
-        $used_EIDL_amount = str_replace(",", "", $used_EIDL_amount);
-        $used_EIDL_amount = floatval(substr($used_EIDL_amount, 2));
-
-        if ($used_EIDL_amount < 0) {
-            $used_EIDL_amount = 0;
-        }
-
-        $loan_amount = $average_payroll * 2.5 - $used_EIDL_amount;
-        if ($loan_amount < 0) {
-            $loan_amount = 0;
-        }
-        return number_format($loan_amount, 2);
-    }
-
-    private function getEIDLAmount($elements) {
-        $used_EIDL_amount = $elements["used_loan_amount"]["#default_value"];
-        if (empty($used_EIDL_amount)) {
-            return "0.00";
-        }
-        $used_EIDL_amount = floatval(substr($used_EIDL_amount, 2));
-        return number_format($used_EIDL_amount, 2);
-    }
+    
+    
+    
 
     
     
