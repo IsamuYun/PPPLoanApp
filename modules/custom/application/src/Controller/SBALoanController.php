93e881ca7ebb277a9ef8e06aa034f169de2a358a
Isamu <yunchunnan@qq.com>
2021-03-24 15:15:04 -0700

    Convert date format

--- modules/custom/application/src/Controller/SBALoanController.php
+++ modules/custom/application/src/Controller/SBALoanController.php
@@ -119,7 +119,8 @@ class SBALoanController {
             if (!empty($sba_loan_amount)) {
                 $sba_loan_amount = number_format($sba_loan_amount, 2);
             }
-
+            $date = strtotime($sba_decision_date);
+            $sba_decision_date = date('m-d-Y', $date);
             //$sba_decision_date = DateTime::createFromFormat('Y-m-d', $sba_decision_date)->format('m-d-Y');
             
             $entity = $form_state->getFormObject()->getEntity();
